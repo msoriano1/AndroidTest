d2caed0b82aaa7ab512b112d81bcbef8cc5bb763
msoriano1 <matthew.soriano@obf.ateneo.edu>
2019-07-11 15:53:27 +0800

    Added notes

--- app/src/main/java/com/mattdog/androidtest/MainActivity.kt
+++ app/src/main/java/com/mattdog/androidtest/MainActivity.kt
@@ -15,5 +15,12 @@ class MainActivity : AppCompatActivity() {
     //VCS tab opens version control settings
     // Select 'Integrate project' and then choose 'Git"
     //Select 'Import into Version Control' and choose 'Share project on Github' to create repo on Github
-    // Select 'Git' and select 'Branches' to add branches
+    // Select 'Git' and select 'Branches' to add/switch branches
+    //Select 'VCS' and select 'Commit' to commit any changes made
+    //Select 'Git' and 'Pull' to pull the most updated version
+    //If there is a merge conflict, select 'Git' and select 'Resolve Conflicts'
+    //Select 'Git' and select 'Merge Changes' to merge changes with pulled branch and local branch
+    //Select 'VCS' and select 'Commit' to commit the merged changes
+    //Select 'Git' and select 'Push' to push the merged version
+
 }
